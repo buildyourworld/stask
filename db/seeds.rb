d74f6e2f2d12b546dbb10e7458cc9a3a57fc9204
monolok <antoinebertin@me.com>
2015-05-02 21:48:35 +0200

    db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "Food")
+Category.create(name: "Medecine")
+Category.create(name: "Politics")
