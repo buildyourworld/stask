75b1cf4b66d8fb75dbac3268308b0f9b3e492dbf
jeanjj <jhbarbecormier@gmail.com>
2015-05-03 17:16:27 +0200

    design ideas

--- db/migrate/20150423202028_create_directions.rb
+++ db/migrate/20150423202028_create_directions.rb
@@ -1,10 +1,10 @@
 class CreateDirections < ActiveRecord::Migration
   def change
     create_table :directions do |t|
-      t.text :step
-      t.belongs_to :recipe, index: true
+    	t.text :step
+    	t.belongs_to :recipe, index: true
 
-      t.timestamps null: false
+    	t.timestamps null: false
     end
     add_foreign_key :directions, :recipes
   end
